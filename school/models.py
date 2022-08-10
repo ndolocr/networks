c1c4b12e5c9438e8d7a20ee60084022a8215f8ef
ndolocr <crndolo@gmail.com>
2022-08-10 14:19:42 +0300

    Creating Student model and associating paren with student

--- school/models.py
+++ school/models.py
@@ -26,12 +26,21 @@ class StudentClass(models.Model):
     def __str__(self):
         return self.class_name
 
+class Parent(models.Model):
+    id_no = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
 class Student(models.Model):
-    id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     admission_no = models.CharField(max_length=255)
+    parent = models.ManyToManyField(Parent, on_delete=models.CASCADE)
     in_class = models.ForeignKey(StudentClass, on_delete=models.CASCADE)
     
     def __str__(self):
-        return self.name
+        return self.first_name + " " + self.last_name
+
