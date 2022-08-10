b3eb4c1fbe3b32057f34454d3568a31b0e14e367
ndolocr <crndolo@gmail.com>
2022-08-10 14:31:33 +0300

    register models in admin file for school app

--- school/admin.py
+++ school/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from school.models import County, School, StudentClass, Parent, Student, Teacher
 
 # Register your models here.
+admin.site.register(County)
+admin.site.register(School)
+admin.site.register(Parent)
+admin.site.register(Teacher)
+admin.site.register(Student)
+admin.site.register(StudentClass)
