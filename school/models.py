47fa60ac91283e1b866decaa150be2192a21fa70
ndolocr <crndolo@gmail.com>
2022-08-10 13:44:06 +0300

    Creating county model

--- school/models.py
+++ school/models.py
@@ -1,3 +1,9 @@
+from typing_extensions import Required
 from django.db import models
 
 # Create your models here.
+class County(models.Model):
+    county_name = models.CharField(max_length=255, null=False)
+
+    def __str__(self):
+        return self.county_name
