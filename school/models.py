034045aa6c50224f27ad2d8695cb236801e380da
ndolocr <crndolo@gmail.com>
2022-08-10 13:50:01 +0300

    Creating model for School and including country choice for County model

--- school/models.py
+++ school/models.py
@@ -3,7 +3,15 @@ from django.db import models
 
 # Create your models here.
 class County(models.Model):
+    COUNTRY_CHOICE = [
+        ("KE", "KE"),
+    ]
     county_name = models.CharField(max_length=255, null=False)
+    country = models.CharField(max_length=10, choices=COUNTRY_CHOICE, default="KE")
 
     def __str__(self):
         return self.county_name
+
+class School(models.Model):
+    school_name = models.CharField(max_length=255, null=False)
+    county = models.ForeignKey(County, on_delete=models.CASCADE)
